9ed07557d8a909c994e1905b8f32d806279c3954
JovemDevv <anacarolinesan18@gmail.com>
2024-01-24 17:26:30 -0300

    tokens commit

---
+++ build/js/tokens.js
@@ -0,0 +1,14 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 24 Jan 2024 19:06:43 GMT
+ */
+
+module.exports = {
+  "VariableCollectionGrey100": "#d9d9d9ff",
+  "VariableCollectionGrey200": "#c1c1c4ff",
+  "VariableCollectionGrey500": "#5b5656ff",
+  "VariableCollectionGrey900": "#1c1c1fff",
+  "VariableCollectionBlack900": "#000000ff",
+  "VariableCollectionPurple700": "#451d9fff",
+  "VariableCollectionWhite": "#ffffffff"
+};
